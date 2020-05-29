dbc6dded62d9a3c52bce5db853177338073d9bf3
“hpshinde” <hshinde@gmx.com>
2020-05-29 17:29:09 +0530

    Add existing file

--- update_git.sh
+++ update_git.sh
@@ -1,1 +1,8 @@
-git add . && git commit -m "Add existing file" && git push origin master
+#!/bin/sh
+while true  
+do
+	git add . &&
+	git commit -m "Add existing file" &&
+	git push origin master
+	sleep 300
+done
